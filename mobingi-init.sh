bd30cbafa7e4daded1207e706dcbcf2504704ea6
Masafumi Noguchi <ngratin@gmail.com>
2017-12-21 14:41:12 +0900

    test

---
+++ mobingi-init.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "-----"
+echo "test2"
+echo "-----"
+touch /tmp/hoge
+touch /tesutofile
+touch /tesutofile2
+
+sleep 30
+touch /first
+sleep 30
+touch /second
+sleep 30
+touch /third
+
+ls foobar
